b14fb5132b7745e9758919e552f51319a83f9855
ijh1298 <imjh0417@gmail.com>
2024-11-10 00:38:37 +0900

    feat: data class Promotion 내 isDateValid 메소드 추가

--- src/main/kotlin/data/model/Promotion.kt
+++ src/main/kotlin/data/model/Promotion.kt
@@ -7,5 +7,10 @@ data class Promotion(
     val buy: Int,
     val get: Int,
     val startDate: LocalDate,
-    val endDate: LocalDate
-)
+    val endDate: LocalDate,
+) {
+    fun isDateValid(targetDate: LocalDate): Boolean {
+        return (targetDate.isEqual(this.startDate) || targetDate.isAfter(this.startDate))
+                && (targetDate.isEqual(this.endDate) || targetDate.isBefore(this.endDate))
+    }
+}
