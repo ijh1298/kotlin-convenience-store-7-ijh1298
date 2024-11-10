816fef15549eecf0ea66ec32b8ed7f059e9f4207
ijh1298 <imjh0417@gmail.com>
2024-11-10 17:13:45 +0900

    feat: ConvenienceStore 내 updateStock 메소드 추가

--- src/main/kotlin/data/ConvenienceStore.kt
+++ src/main/kotlin/data/ConvenienceStore.kt
@@ -1,6 +1,8 @@
 package data
 
+import data.model.Product
 import data.model.Promotion
+import data.model.StockChange
 import data.repository.ProductRepository
 import data.repository.PromotionRepository
 
@@ -10,4 +12,28 @@ object ConvenienceStore {
 
     val promotions = promotionRepository.fetchPromotions().toMutableList()
     val products = productRepository.fetchProducts().toMutableList()
+
+    fun updateStock(stockChange: StockChange) {
+        val (itemName, purchaseInfos) = stockChange
+        var sellAmount = purchaseInfos.map { it.buyQuantity + it.getQuantity }.sum()
+        val promoStock = products.find { it.name == itemName && it.promotion != null }
+        val normalStock = products.find { it.name == itemName && it.promotion == null }
+
+        val remainder = updatePromoStockGetRemainder(promoStock, sellAmount)
+        updateNormalStock(normalStock, remainder)
+    }
+
+    private fun updatePromoStockGetRemainder(promo: Product?, sellAmount: Int): Int {
+        if (promo == null || promo.quantity == 0)
+            return sellAmount
+        promo.quantity = maxOf(promo.quantity - sellAmount, 0)
+        return maxOf(sellAmount - promo.quantity, 0)
+    }
+
+    private fun updateNormalStock(normal: Product?, sellAmount: Int) {
+        if (normal == null || normal.quantity == 0)
+            return
+        normal.quantity = maxOf(normal.quantity - sellAmount, 0)
+        return
+    }
 }
