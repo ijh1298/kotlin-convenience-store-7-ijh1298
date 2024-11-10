e1a5cb94cff4d7fe08864e2ff8e7184b0ab9da2f
ijh1298 <imjh0417@gmail.com>
2024-11-10 16:19:37 +0900

    feat: ConvenienceStoreService, ConvenienceStoreController 내 물품 구매 비즈니스 로직 추가

--- src/main/kotlin/controller/ConvenienceStoreController.kt
+++ src/main/kotlin/controller/ConvenienceStoreController.kt
@@ -1,19 +1,81 @@
 package controller
 
 import data.ConvenienceStore
+import data.model.PurchaseInfo
+import data.model.PurchaseResult
+import data.model.StockChange
+import service.ConvenienceStoreService
+import util.PurchaseStatus
 import validator.InputItemValidator
 import view.InputView
 import view.OutputView
+import java.security.Provider.Service
 
 class ConvenienceStoreController(
     private val inputView: InputView,
     private val outputView: OutputView,
 ) {
     private lateinit var buyItems: List<Pair<String, Int>>
+    private lateinit var toBeUpdatedStocks: List<StockChange>
+    private var needPromotion: Boolean = false
+    private var okWithoutPromo: Boolean = false
 
     fun run() {
         outputView.showStock(ConvenienceStore.products)
         loopUntilValid { tryInputItem() }
+        buyItems()
+    }
+
+    fun buyItems() {
+        toBeUpdatedStocks = getAllStockChanges()
+        println(toBeUpdatedStocks)
+        // Service.updateStocks(toBeUpdatedStocks)
+        // Service.가격 반영
+    }
+
+    private fun getAllStockChanges(): List<StockChange> {
+        val purchaseInfos = mutableListOf<StockChange>()
+        buyItems.forEach { (item, buyQuantity) ->
+            val itemPurchaseInfo = getEachProductPurchaseInfos(item, buyQuantity)
+            purchaseInfos += StockChange(item, itemPurchaseInfo)
+        }
+        return purchaseInfos.toList()
+    }
+
+    private fun getEachProductPurchaseInfos(itemName: String, buyQuantity: Int): List<PurchaseInfo> {
+        val (promoQuantity, normalQuantity) = ConvenienceStoreService.separateBuyingQuantities(itemName, buyQuantity)
+        val promoPurchaseInfo = getEachPurchasedPromoItem(itemName, promoQuantity)
+        val normalPurchaseInfo = getEachPurchasedNormalItem(itemName, normalQuantity)
+
+        if (promoPurchaseInfo.buyQuantity == 0) return listOf(normalPurchaseInfo) // promo 상품이 null인 경우
+        if (promoPurchaseInfo.stopPurchase) return listOf(promoPurchaseInfo)
+        return listOf(promoPurchaseInfo, normalPurchaseInfo)
+    }
+
+    private fun getEachPurchasedNormalItem(itemName: String, buyQuantity: Int): PurchaseInfo =
+        PurchaseInfo(buyQuantity, 0, false)
+
+    private fun getEachPurchasedPromoItem(itemName: String, buyQuantity: Int): PurchaseInfo {
+        val promoPurchaseResult = ConvenienceStoreService.processToGetPromoPurchaseResult(itemName, buyQuantity)
+
+        return when (promoPurchaseResult.status) {
+            PurchaseStatus.REQUEST_EXTRA_RESPONSE -> requestExtraGet(itemName, buyQuantity)
+            PurchaseStatus.REQUEST_WITHOUT_PROMO -> requestWithoutPromo(itemName, buyQuantity)
+            PurchaseStatus.SUCCESS_WITHOUT_PROMO -> PurchaseInfo(buyQuantity, 0, false) // 일반적인 구매
+            PurchaseStatus.SUCCESS -> ConvenienceStoreService.getPromoSuccessResult(itemName, buyQuantity)
+            else -> PurchaseInfo(0, 0, false)
+        }
+    }
+
+    private fun requestExtraGet(itemName: String, buyQuantity: Int): PurchaseInfo {
+        loopUntilValid { tryInputNeedPromotion() }
+        return ConvenienceStoreService.getResultByExtraResponse(itemName, buyQuantity, needPromotion)
+    }
+
+    private fun requestWithoutPromo(itemName: String, buyQuantity: Int): PurchaseInfo {
+        loopUntilValid { tryInputBuyWithoutPromotion() }
+        val (promoQuantity, normalQuantity) = ConvenienceStoreService.separateBuyingQuantities(itemName, buyQuantity)
+        return ConvenienceStoreService.getResultByPromoProcess(itemName, promoQuantity, normalQuantity, okWithoutPromo)
     }
 
     private fun tryInputItem(): Boolean {
@@ -21,6 +83,30 @@ class ConvenienceStoreController(
             val inputItem = inputView.inputItem()
             InputItemValidator.validate(inputItem)
             buyItems = inputItem.toBuyItemsList()
+            return true
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+            return false
+        }
+    }
+
+    private fun tryInputNeedPromotion(): Boolean {
+        try {
+            val inputNeedPromotion = inputView.inputNeedPromotion()
+            require(inputNeedPromotion == "Y" || inputNeedPromotion == "N") { "[ERROR] 잘못된 입력입니다. 다시 입력해 주세요." }
+            needPromotion = inputNeedPromotion.answerToBoolean()
+            return true
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+            return false
+        }
+    }
+
+    private fun tryInputBuyWithoutPromotion(): Boolean {
+        try {
+            val inputBuyWithoutPromotion = inputView.inputBuyWithoutPromotion()
+            require(inputBuyWithoutPromotion == "Y" || inputBuyWithoutPromotion == "N") { "[ERROR] 잘못된 입력입니다. 다시 입력해 주세요." }
+            okWithoutPromo = inputBuyWithoutPromotion.answerToBoolean()
             return true
         } catch (e: IllegalArgumentException) {
             println(e.message)
@@ -39,6 +125,12 @@ class ConvenienceStoreController(
             val unparsedItems = this.replace("[", "").replace("]", "").split(',')
             val pairedItems = unparsedItems.map { it.split('-') }.map { it[NAME_IDX] to it[QUANTITY_IDX].toInt() }
             return pairedItems
+        }
+
+        private fun String.answerToBoolean(): Boolean {
+            if (this == "Y")
+                return true
+            return false
         }
 
         private const val NAME_IDX = 0
--- src/main/kotlin/service/ConvenienceStoreService.kt
+++ src/main/kotlin/service/ConvenienceStoreService.kt
@@ -1,25 +1,101 @@
 package service
 
+import camp.nextstep.edu.missionutils.DateTimes
 import data.ConvenienceStore.products
 import data.ConvenienceStore.promotions
+import data.model.Product
 import data.model.Promotion
+import data.model.PurchaseInfo
+import data.model.PurchaseResult
+import util.PurchaseStatus
 
 object ConvenienceStoreService {
-    private fun checkBuyItems(buyItems: List<Pair<String, Int>>) {
-        buyItems.forEach { item ->
-
-        }
+    fun processToGetPromoPurchaseResult(itemName: String, promoBuyQuantity: Int): PurchaseResult {
+        val promoProduct = getPromoProduct(itemName)
+        if (promoProduct == null)
+            return PurchaseResult(PurchaseStatus.FAILURE_NULL_PRODUCT, promoProduct)
+        return getPromoPurchaseResult(promoProduct, promoBuyQuantity)
     }
+
+    fun getPromoSuccessResult(itemName: String, buyQuantity: Int): PurchaseInfo {
+        val (_, nBuy, nGet, _, _) = getPromotion(itemName)!!
+        val freeQuantity = buyQuantity / (nBuy + nGet)
+        return PurchaseInfo(buyQuantity - freeQuantity, freeQuantity, false)
+    }
+
+    fun getResultByPromoProcess(
+        itemName: String,
+        promoQuantity: Int,
+        normalQuantity: Int,
+        response: Boolean,
+    ): PurchaseInfo {
+        val (_, nBuy, nGet, _, _) = getPromotion(itemName)!!
+        val freeQuantity = promoQuantity / (nBuy + nGet)
+        val payQuantity = freeQuantity * nBuy
+
+        if (response)
+            return PurchaseInfo(promoQuantity - freeQuantity, freeQuantity, false) // 구매하고, 최초 함수에서 일반 구매 로직까지 계속 진행
+        return PurchaseInfo(payQuantity, freeQuantity, true) // 프로모션 적용 품목만 구매, 최초 함수 돌아가면 중단
+    }
+
+    fun getResultByExtraResponse(itemName: String, buyQuantity: Int, response: Boolean): PurchaseInfo {
+        val (_, nBuy, nGet, _, _) = getPromotion(itemName)!!
+        val freeQuantity = buyQuantity / (nBuy + nGet)
+        val payQuantity = freeQuantity * nBuy
+
+        if (response)
+            return PurchaseInfo(buyQuantity - freeQuantity, freeQuantity + nGet, false)
+        return PurchaseInfo(buyQuantity - freeQuantity, freeQuantity, false)
+    }
+
+    private fun getPromoPurchaseResult(product: Product, promoQuantity: Int): PurchaseResult {
+        if (!product.promotion!!.isDateValid(DateTimes.now().toLocalDate())) // 프로모션 기간이 아니면 일반 구매
+            return PurchaseResult(PurchaseStatus.SUCCESS_WITHOUT_PROMO, product)
+        return getValidPromoPurchaseResult(product, promoQuantity)
+    }
+
+    private fun getValidPromoPurchaseResult(product: Product, promoQuantity: Int): PurchaseResult {
+        val (_, _, stock, promo) = product
+        val (_, nBuy, nGet, _, _) = promo!!
+
+        if (canGetExtra(promoQuantity, stock, nBuy, nGet))
+            return PurchaseResult(PurchaseStatus.REQUEST_EXTRA_RESPONSE, product)
+        if (needPromotionProcess(promoQuantity, stock, nBuy, nGet))
+            return PurchaseResult(PurchaseStatus.REQUEST_WITHOUT_PROMO, product)
+        return PurchaseResult(PurchaseStatus.SUCCESS, product)
+    }
+
+    private fun getPromotion(itemName: String): Promotion? {
+        val product = getPromoProduct(itemName) ?: return null
+        return product.promotion
+    }
+
+    private fun canGetExtra(buyQuantity: Int, stock: Int, nBuy: Int, nGet: Int): Boolean =
+        buyQuantity % (nBuy + nGet) == nBuy && stock >= buyQuantity + nGet
+
+    private fun needPromotionProcess(buyQuantity: Int, stock: Int, nBuy: Int, nGet: Int): Boolean =
+        stock == buyQuantity && buyQuantity % (nBuy + nGet) != 0 // 지급할 프로모션 재고가 없는 경우
 
     fun getItemNames(): List<String> = products.map { it.name }
 
     fun getPromotionNames(): List<String> = promotions.map { it.name }
 
-    fun getQuantity(productName: String): Int {
-        return products.filter { it.name == productName }.sumOf { it.quantity }
-    }
+    fun getQuantity(productName: String): Int = products.filter { it.name == productName }.sumOf { it.quantity }
 
-    fun getPromotionByName(promotionName: String): Promotion? {
-        return promotions.find { it.name == promotionName }
+    fun getPromotionByName(promotionName: String): Promotion? = promotions.find { it.name == promotionName }
+
+    private fun getPromoProduct(productName: String) = products.find { it.name == productName && it.promotion != null }
+
+    private fun getNormalProduct(productName: String) = products.find { it.name == productName && it.promotion == null }
+
+    // **Validator에서 buyQuantity만큼 구매가 가능함을 이미 확인한 상태임.**
+    fun separateBuyingQuantities(productName: String, buyQuantity: Int): Pair<Int, Int> {
+        val stock = getPromoProduct(productName) to getNormalProduct(productName)
+
+        if (stock.first == null) return 0 to buyQuantity
+        if (stock.second == null) return buyQuantity to 0
+
+        val promoQuantity = minOf(buyQuantity, stock.first!!.quantity)
+        return promoQuantity to buyQuantity - promoQuantity
     }
 }
