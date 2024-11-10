67d8bdd447db24a8ae0764404cf9b94664430af6
ijh1298 <imjh0417@gmail.com>
2024-11-10 16:16:25 +0900

    fix: ProductValidator가 null Promotion을 예외 처리하지 않도록 수정

--- src/main/kotlin/validator/ProductValidator.kt
+++ src/main/kotlin/validator/ProductValidator.kt
@@ -8,7 +8,6 @@ object ProductValidator {
             require(product.size == 4) { "[ERROR] Product 문자열이 유효하지 않습니다." }
             require(product[1].all { it.isDigit() }) { "[ERROR] Product 가격이 유효한 정수가 아닙니다." }
             require(product[2].all { it.isDigit() }) { "[ERROR] Product 재고가 유효한 정수가 아닙니다." }
-            require(product[3] in ConvenienceStoreService.getPromotionNames()) { "[ERROR] Product Promotion이 존재하지 않습니다." }
             true
         } catch (e: IllegalArgumentException) {
             false
