6b483a153a74e58bf5847838683cd8b45abcfc0b
ijh1298 <imjh0417@gmail.com>
2024-11-10 17:12:38 +0900

    feat: InputView 내 inputTryAgain 메소드 추가

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -22,4 +22,9 @@ class InputView {
         println("멤버십 할인을 받으시겠습니까? (Y/N)")
         return Console.readLine()
     }
+
+    fun inputTryAgain(): String {
+        println("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)")
+        return Console.readLine()
+    }
 }
